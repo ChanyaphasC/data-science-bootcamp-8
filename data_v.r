5e078392b3df04c8f12c6da809d6ad886c9329ff
Chanyapas Chaikongcha <chanyapas2545@gmail.com>
2023-09-09 14:05:13 +0000

    add second plot

--- data_v.r
+++ data_v.r
@@ -2,5 +2,11 @@
 # update at this file, just another comment
 library(ggplot2)
 
+# first plot
 ggplot(diamonds, aes(carat, price)) +
   geom_point()
+
+# second plot
+ggplot(diamonds, aes(carat, price)) +
+  geom_point() +
+  geom_smooth()
